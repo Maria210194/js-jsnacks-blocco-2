467d519e78ede4c03922ee36dd79a2235c482b4b
Maria Stachie <mary.stachie@gmail.com>
2022-03-11 15:42:53 +0100

    prova

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,1 +1,8 @@
-console.log('OK JS');
+const interi = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+let somma = 0;
+
+for (let i = 0; i < interi.length; i++) {
+    const elementiArray = interi[i];
+    somma += elementiArray[0 + 1];
+    console.log('somma');
+}
