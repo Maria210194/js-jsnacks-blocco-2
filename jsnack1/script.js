4b7d8105c735d7edd2fe339d7e0e9fc94f264e6f
Maria Stachie <mary.stachie@gmail.com>
2022-03-11 17:50:51 +0100

    first es done

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,8 +1,12 @@
-const interi = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+// Crea un array di numeri interi e somma quelli che si trovano nelle posizioni dispari
+
+const interi = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let somma = 0;
 
-for (let i = 0; i < interi.length; i++) {
-    const elementiArray = interi[i];
-    somma += elementiArray[0 + 1];
-    console.log('somma');
+for (i = 0; i < interi.length; i++) {
+    if (i % 2 !== 0) {
+        somma += interi[i];
+    }
+
 }
+console.log(somma);
