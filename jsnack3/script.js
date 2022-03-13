5d66863e46dcfed8bc70bd434d42f4d3017df08a
Maria Stachie <mary.stachie@gmail.com>
2022-03-13 11:08:21 +0100

    es 3 done

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -3,14 +3,15 @@ console.log('OK JS');
 const primo = [77, 30, 100, 7, 5, 21, 68];
 const secondo = [2, 5];
 let check = false;
-const casuale = Math.floor(Math.random() * 8);
 
-for (i = 0; i = primo.length; i++) {
+for (i = 0; i <= primo.length; i++) {
     if (primo.length > secondo.length) {
+        let casuale = Math.floor(Math.random() * 100);
         secondo.push(casuale);
+        check = false;
     }
 }
 if (primo.length = secondo.length) {
     check = true;
-    console.log(primo, secondo);
+    console.log(secondo, primo);
 }
